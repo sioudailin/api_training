0780834b5e0a7d02dbeeadb5db74e1cd4880b732
Jeff Cheng <jeffcheng777@gmail.com>
2022-02-19 22:25:34 +0800

    second version

--- jokes.py
+++ jokes.py
@@ -2,7 +2,6 @@
 # 將笑話總數印出，若超過一筆，隨機取一筆 show 給使用者看。
 
 # Packages
-import re
 import requests
 import json
 import random
@@ -14,8 +13,13 @@ term = input("Please enter what you want to search? ").lower()
 
 # version 2
 response = requests.get(url, headers={"Accept": "application/json"}, params={"term": f"{term}"})
+
 response = json.loads(response.text)
-result = random.choice(response["results"])["joke"]
-total_jokes = response["total_jokes"]
-print(f"I found {total_jokes}. And here's one for you! \n {result}")
+results = response["results"]
+if results:
+    result = random.choice(results)["joke"]
+    total_jokes = response["total_jokes"]
+    print(f"I found {total_jokes}. And here's one for you! \n {result}")
+else:
+    print("I found nothing!!!")
 
